dfd65b2a03a015411a3986766fbf47ddbbfc9a60
bresden5 <125457408+bresden5@users.noreply.github.com>
2026-02-02 20:32:38 +0100

    Upload

--- Huawei Fusion Solar/module.php
+++ Huawei Fusion Solar/module.php
@@ -1,23 +1,196 @@
 <?php
 
-declare(strict_types=1);
-	class HuaweiFusionSolar extends IPSModule
-	{
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
-		}
+class HuaweiFusionSolar extends IPSModule
+{
+    public function Create()
+    {
+        parent::Create();
 
-		public function Destroy()
-		{
-			//Never delete this line!
-			parent::Destroy();
-		}
+        // Properties
+        $this->RegisterPropertyString("Username", "");
+        $this->RegisterPropertyString("Password", "");
+        $this->RegisterPropertyInteger("UpdateInterval", 300);
 
-		public function ApplyChanges()
-		{
-			//Never delete this line!
-			parent::ApplyChanges();
-		}
-	}
+        // Timer
+        $this->RegisterTimer(
+            "UpdateTimer",
+            0,
+            "HFS_Update($_IPS['TARGET']);"
+        );
+
+        // Variables
+        $this->RegisterVariableFloat("TotalPV", "PV Gesamtleistung", "~Watt");
+        $this->RegisterVariableFloat("HouseConsumption", "Hausverbrauch", "~Watt");
+
+        $this->RegisterVariableFloat("GridImport", "Netzbezug", "~Watt");
+        $this->RegisterVariableFloat("GridExport", "Netzeinspeisung", "~Watt");
+
+        $this->RegisterVariableFloat("BatterySOC", "Batterie Ladestand", "~Percent");
+        $this->RegisterVariableFloat("BatteryPower", "Batterie Leistung", "~Watt");
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+
+        $interval = $this->ReadPropertyInteger("UpdateInterval") * 1000;
+        $this->SetTimerInterval("UpdateTimer", $interval);
+    }
+
+    public function Update()
+    {
+        try {
+            if ($this->GetBuffer("Token") === "") {
+                $this->Login();
+            }
+
+            $stationId = $this->GetStationId();
+            $devices   = $this->GetDevices($stationId);
+
+            $pvTotal = 0;
+            $gridPower = 0;
+            $batPower  = 0;
+            $batSOC    = 0;
+
+            foreach ($devices as $dev) {
+
+                $real = $this->GetDeviceRealTime($dev['id']);
+                $map  = $real['data'][0]['dataItemMap'] ?? [];
+
+                switch ($dev['devTypeId']) {
+
+                    // Wechselrichter
+                    case 1:
+                        $power = $map['active_power'] ?? 0;
+                        $pvTotal += $power;
+
+                        $ident = "INV_" . $dev['id'];
+                        if (!$this->GetIDForIdent($ident)) {
+                            $this->RegisterVariableFloat(
+                                $ident,
+                                "WR " . $dev['devName'],
+                                "~Watt"
+                            );
+                        }
+                        $this->SetValue($ident, $power);
+                        break;
+
+                    // Batterie
+                    case 39:
+                        $batSOC   = $map['soc'] ?? 0;
+                        $batPower = $map['charge_discharge_power'] ?? 0;
+                        break;
+
+                    // Netz-Zähler
+                    case 47:
+                        $gridPower = $map['active_power'] ?? 0;
+                        break;
+                }
+            }
+
+            // Werte setzen
+            $this->SetValue("TotalPV", $pvTotal);
+            $this->SetValue("BatterySOC", $batSOC);
+            $this->SetValue("BatteryPower", $batPower);
+
+            $this->SetValue("GridImport", max($gridPower, 0));
+            $this->SetValue("GridExport", max(-$gridPower, 0));
+
+            // Hausverbrauch berechnen
+            $house =
+                $pvTotal
+                + max($gridPower, 0)
+                - max(-$gridPower, 0)
+                - max($batPower, 0)
+                + max(-$batPower, 0);
+
+            $this->SetValue("HouseConsumption", $house);
+
+            $this->SetStatus(102);
+
+        } catch (Throwable $e) {
+            IPS_LogMessage("FusionSolar", $e->getMessage());
+            $this->SetStatus(201);
+        }
+    }
+
+    /* -------------------- API -------------------- */
+
+    private function Login()
+    {
+        $response = $this->ApiRequest("/thirdData/login", [
+            "userName"    => $this->ReadPropertyString("Username"),
+            "systemCode"  => $this->ReadPropertyString("Password")
+        ]);
+
+        if (!isset($response['data'])) {
+            throw new Exception("FusionSolar Login fehlgeschlagen");
+        }
+
+        $this->SetBuffer("Token", $response['data']);
+    }
+
+    private function GetStationId(): string
+    {
+        if ($this->GetBuffer("StationID") !== "") {
+            return $this->GetBuffer("StationID");
+        }
+
+        $response = $this->ApiRequest("/thirdData/getStationList");
+
+        if (!isset($response['data'][0]['stationCode'])) {
+            throw new Exception("Keine Station gefunden");
+        }
+
+        $stationId = $response['data'][0]['stationCode'];
+        $this->SetBuffer("StationID", $stationId);
+
+        return $stationId;
+    }
+
+    private function GetDevices(string $stationId): array
+    {
+        $response = $this->ApiRequest("/thirdData/getDevList", [
+            "stationCodes" => [$stationId]
+        ]);
+
+        return $response['data'] ?? [];
+    }
+
+    private function GetDeviceRealTime(string $devId): array
+    {
+        return $this->ApiRequest("/thirdData/getDevRealKpi", [
+            "devIds" => [$devId]
+        ]);
+    }
+
+    private function ApiRequest(string $endpoint, array $payload = []): array
+    {
+        $url = "https://eu5.fusionsolar.huawei.com" . $endpoint;
+        $token = $this->GetBuffer("Token");
+
+        $ch = curl_init($url);
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST           => true,
+            CURLOPT_POSTFIELDS     => json_encode($payload),
+            CURLOPT_HTTPHEADER     => [
+                "Content-Type: application/json",
+                "XSRF-TOKEN: " . $token
+            ]
+        ]);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $data = json_decode($response, true);
+
+        if (($data['failCode'] ?? 0) === 305) {
+            $this->SetBuffer("Token", "");
+            $this->Login();
+            return $this->ApiRequest($endpoint, $payload);
+        }
+
+        return $data ?? [];
+    }
+}
